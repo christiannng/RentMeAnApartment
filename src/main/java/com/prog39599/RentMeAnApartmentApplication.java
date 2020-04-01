707178e2b8cc86860940a45b6ff2c4d8daad9fa2
Christian Nguyen <christiannguyen110591@gmail.com>
2020-03-31 23:22:47 -0400

    [Chrisitan] Initial commit

---
+++ src/main/java/com/prog39599/RentMeAnApartmentApplication.java
@@ -0,0 +1,13 @@
+package com.prog39599;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentMeAnApartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentMeAnApartmentApplication.class, args);
+	}
+
+}
